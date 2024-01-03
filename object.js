36760bc35c20506292d866b3d434bf2dfa69e071
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-03 17:29:15 +0700

    Buổi 1: Ôn tập về DOM

---
+++ object.js
@@ -0,0 +1,10 @@
+console.log("hello vietnam");
+
+var myInfor = {
+  myName: "Nguyễn Minh Hoàng",
+  age: 32,
+  address: "Bắc Giang",
+  mssv: "PH12345",
+  email: "hoangnm62@gmail.com",
+  position: "Bảo vệ và trông xe",
+};
