049c95750768ad3c433d13d3ed1f216f27f4cb46
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-03 17:45:26 +0700

    add object.js

--- object.js
+++ object.js
@@ -8,3 +8,18 @@ var myInfor = {
   email: "hoangnm62@gmail.com",
   position: "Bảo vệ và trông xe",
 };
+
+// Gợi ý:
+/**
+ * Bước 1: Lấy element trong DOM ra.
+ * Bước 2: Dùng createElement để tạo ra element mới.
+ * Bước 3: Thêm nội dung cho element mới này bằng innerText, innerHTML
+ * Bước 4: Dùng appendChild để thêm phần tử đó vào DOM.
+ */
+
+var inforElement = document.getElementsByClassName("infor")[0]; // Bước 1
+console.log(inforElement);
+
+var inforHTML = document.createElement("div"); // Bước 2
+inforHTML.innerText = myInfor.myName; // Bước 3
+inforElement.appendChild(inforHTML);
