6ee0d10096c65b0992190a52c21b3ed77f54eedd
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-05 18:00:53 +0700

    Buổi 2: register form -DONE

--- event.js
+++ event.js
@@ -1,21 +1,21 @@
-// function login() {
-//   console.log("Dang nhap thanh cong!");
-// }
-
-// var loginCach2 = document.getElementById("cach2");
-// console.log(loginCach2);
-// console.log(typeof loginCach2);
-
-// loginCach2.onclick = login;
-
-// var loginCach3 = document.getElementById("cach3");
-
-// loginCach3.addEventListener("click", login);
-
 function register() {
-  // code logic
   /**
    * Lấy dữ liệu khi ấn nút đăng ký lưu vào một object tên là user
    * Lưu thông tin đăng ký vào localStorage
    */
+
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var confirmPass = document.getElementById("confirmPass").value;
+  var user = {
+    email,
+    password,
+    confirmPass,
+  };
+  // console.log({ user });
+
+  // console.log({ userJSON: typeof JSON.stringify(user) });
+  // BOM = Browser Object Model
+  localStorage.setItem("userInfor", JSON.stringify(user));
+  // JSON = Javascript Object Notation
 }
