cc16a2c37c3629e575d368f0fc739e0439892998
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-05 17:22:28 +0700

    Buổi 2: Bài tập register

--- event.js
+++ event.js
@@ -1,13 +1,21 @@
-function login() {
-  console.log("Dang nhap thanh cong!");
+// function login() {
+//   console.log("Dang nhap thanh cong!");
+// }
+
+// var loginCach2 = document.getElementById("cach2");
+// console.log(loginCach2);
+// console.log(typeof loginCach2);
+
+// loginCach2.onclick = login;
+
+// var loginCach3 = document.getElementById("cach3");
+
+// loginCach3.addEventListener("click", login);
+
+function register() {
+  // code logic
+  /**
+   * Lấy dữ liệu khi ấn nút đăng ký lưu vào một object tên là user
+   * Lưu thông tin đăng ký vào localStorage
+   */
 }
-
-var loginCach2 = document.getElementById("cach2");
-console.log(loginCach2);
-console.log(typeof loginCach2);
-
-loginCach2.onclick = login;
-
-var loginCach3 = document.getElementById("cach3");
-
-loginCach3.addEventListener("click", login);
