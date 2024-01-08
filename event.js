39edafb65a6c4c3ae7310dbff655aae0329803ef
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-08 16:52:58 +0700

    Buổi 3: register form validation -Done

--- event.js
+++ event.js
@@ -1,9 +1,21 @@
+var users = [];
+function validationForm(user) {
+  if (!user.email || !user.password || !user.confirmPass) {
+    alert("Khong duoc de trong!");
+    return false;
+  }
+  if (user.password.length < 6) {
+    alert("Password phai dai hon 6 ky tu!");
+    return false;
+  }
+  if (user.confirmPass !== user.password) {
+    alert("Mat khau nhap lai khong khop!");
+    return false;
+  }
+  // validate
+  return true;
+}
 function register() {
-  /**
-   * Lấy dữ liệu khi ấn nút đăng ký lưu vào một object tên là user
-   * Lưu thông tin đăng ký vào localStorage
-   */
-
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
   var confirmPass = document.getElementById("confirmPass").value;
@@ -12,10 +24,10 @@ function register() {
     password,
     confirmPass,
   };
-  // console.log({ user });
 
-  // console.log({ userJSON: typeof JSON.stringify(user) });
-  // BOM = Browser Object Model
-  localStorage.setItem("userInfor", JSON.stringify(user));
-  // JSON = Javascript Object Notation
+  if (validationForm(user)) {
+    users.push(user);
+    localStorage.setItem("users", JSON.stringify(users));
+    alert("Dang ky thanh cong!");
+  }
 }
